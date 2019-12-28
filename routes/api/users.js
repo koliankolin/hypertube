c43a588fc2fde81936dfd92b0d2c5b7f18f0a9df
gorchakov.s <gorchakov.s@roistat.com>
2019-12-28 15:59:23 +0300

    change password and utilities

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,8 @@ const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const auth = require('../../middleware/auth');
+const utilities = require('../../library/utilities');
 
 const {check, validationResult} = require('express-validator');
 
@@ -81,6 +83,46 @@ router.post('/', [
         response.status(500).send('Server error');
     }
 
+});
+
+// @route  POST api/users/password/change
+// @desc   Change password
+// @access Private
+router.post('/password/change', auth, async (req, res) => {
+    let err = [];
+    const { password, cfpassword } = req.body;
+    err.push({
+        filed: 'password',
+        error: await utilities.checkInfo('password', password)
+    });
+    err.push({
+        filed: 'cfpassword',
+        error: await utilities.checkInfo('cfpassword', password, cfpassword)
+    });
+    err = err.filter(e => e.error);
+    if (err.length === 0){
+        const salt = await bcrypt.genSalt(10);
+        const user = await User.findById(req.user.id).select('-password');
+        user.password = await bcrypt.hash(password, salt);
+        await user.save();
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        await jwt.sign(
+            payload,
+            config.get('jwtSecret'),
+            { expiresIn: 3600 },
+            (err, token) =>
+            {
+                if (err) throw err;
+                res.json({ token });
+            });
+    } else
+        return res.send({error : err})
 });
 
 module.exports = router;
