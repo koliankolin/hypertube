97c02121dc4c047660f0200530de651628f8fd84
fwelpe <arestovfa@gmail.com>
2020-02-09 18:57:14 +0300

    yarn -> npm, переделан маршрут токена

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -18,7 +18,7 @@ router.route('/42') //42 Auth
 
 router.route('/42/callback')
     .get(passport.authenticate('42', {failureRedirect: '/'}), (req, res) => {
-        res.redirect(`http://localhost:3000?token=${createToken(req.user, false)}`);
+        res.redirect(`http://localhost:3000/token/${createToken(req.user, false)}`);
     });
 
 router.route('/insta') //42 Auth
@@ -26,7 +26,7 @@ router.route('/insta') //42 Auth
 
 router.route('/insta/callback')
     .get(passport.authenticate('instagram', {failureRedirect: '/'}), (req, res) => {
-        res.redirect(`http://localhost:3000?token=${createToken(req.user, false)}`);
+        res.redirect(`http://localhost:3000/token/${createToken(req.user, false)}`);
     });
 
 // @route  GET api/auth
